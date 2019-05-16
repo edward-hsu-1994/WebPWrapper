44c00bc24bc5290118165a5edac9ee6a635ae0c3
XuPeiYao <st89382000@gmail.com>
2019-05-16 23:36:26 +0800

    加入轉檔Process主程式

--- WebPWrapper/Encoder/WebPEncoder.cs
+++ WebPWrapper/Encoder/WebPEncoder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,8 +18,50 @@ namespace WebPWrapper.Encoder {
         }
 
         public void Encode(Stream input, Stream output) {
-            throw new NotImplementedException();
+            var inputFile = Path.GetTempFileName();
+            var outputFile = Path.GetTempFileName();
+
+            input.Seek(0, SeekOrigin.Begin);
+
+            using (var inputStream = File.Open(inputFile, FileMode.Open)) {
+                input.CopyTo(inputStream);
+            }
+
+            try {
+                using (Process webpProcess = new Process()) {
+                    var stdout = "";
+                    try {
+                        webpProcess.StartInfo.UseShellExecute = false;
+                        webpProcess.StartInfo.FileName = _executeFilePath;
+                        webpProcess.StartInfo.Arguments = _arguments + $" -o {outputFile} {inputFile}";
+                        webpProcess.StartInfo.CreateNoWindow = true;
+                        webpProcess.OutputDataReceived += (object sender, DataReceivedEventArgs e) => {
+                            stdout += e.Data;
+                        };
+                        webpProcess.ErrorDataReceived += (object sender, DataReceivedEventArgs e) => {
+                            stdout += e.Data;
+                        };
+                        webpProcess.Start();
+                        webpProcess.WaitForExit();
+                    } catch (Exception e) {
+                        File.Delete(inputFile);
+                        File.Delete(outputFile);
+
+                        throw new Exception(stdout);
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            using (var outputStream = File.Open(outputFile, FileMode.Open)) {
+                outputStream.CopyTo(output);
+            }
+
+            File.Delete(inputFile);
+            File.Delete(outputFile);
         }
+
 
         public async Task EncodeAsync(Stream input, Stream output) {
             await Task.Run(() => Encode(input, output));
