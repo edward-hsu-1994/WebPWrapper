c924c9825212c9d537884b489727f80fa6388442
XuPeiYao <st89382000@gmail.com>
2019-05-18 00:40:53 +0800

    修正介面方法回傳類型

--- WebPWrapper/Encoder/IWebPEncoderBuilder.cs
+++ WebPWrapper/Encoder/IWebPEncoderBuilder.cs
@@ -10,57 +10,57 @@ namespace WebPWrapper.Encoder {
         /// <param name="y">起始座標Y</param>
         /// <param name="width">寬度</param>
         /// <param name="height">高度</param> 
-        WebPEncoderBuilder Crop(int x, int y, int width, int height);
+        IWebPEncoderBuilder Crop(int x, int y, int width, int height);
 
         /// <summary>
         /// 縮放圖片，<paramref name="height"/>與<paramref name="width"/>至少一者非0，如果其中一值為0則等比例縮放
         /// </summary>
         /// <param name="width">寬度</param>
         /// <param name="height">寬度</param> 
-        WebPEncoderBuilder Resize(int width, int height);
+        IWebPEncoderBuilder Resize(int width, int height);
 
         /// <summary>
         /// 容許多執行序
         /// </summary>
-        WebPEncoderBuilder MultiThread();
+        IWebPEncoderBuilder MultiThread();
 
         /// <summary>
         /// 降低記憶體使用
         /// </summary>
-        WebPEncoderBuilder LowMemory();
+        IWebPEncoderBuilder LowMemory();
 
         /// <summary>
         /// 複製來源圖片的Metadata
         /// </summary>
-        WebPEncoderBuilder CopyMetadata(params Metadatas[] metadatas);
+        IWebPEncoderBuilder CopyMetadata(params Metadatas[] metadatas);
 
         /// <summary>
         /// 停用ASM優化
         /// </summary>
-        WebPEncoderBuilder DisableAssemblyOptimization();
+        IWebPEncoderBuilder DisableAssemblyOptimization();
 
         /// <summary>
         /// 讀取預設的組態
         /// </summary>
         /// <param name="profile">組態類型</param> 
-        WebPEncoderBuilder LoadPresetProfile(PresetProfiles profile);
+        IWebPEncoderBuilder LoadPresetProfile(PresetProfiles profile);
 
         /// <summary>
         /// 設定壓縮組態
         /// </summary>
         /// <param name="config">壓縮組態設定</param> 
-        WebPEncoderBuilder CompressionConfig(Expression<Action<CompressionConfiguration>> config);
+        IWebPEncoderBuilder CompressionConfig(Expression<Action<CompressionConfiguration>> config);
 
         /// <summary>
         /// 設定Alpha組態
         /// </summary>
         /// <param name="config">Alpha組態設定</param>
-        WebPEncoderBuilder AlphaConfig(Expression<Action<AlphaConfiguration>> config);
+        IWebPEncoderBuilder AlphaConfig(Expression<Action<AlphaConfiguration>> config);
 
         /// <summary>
         /// 重設回預設值
         /// </summary>
-        WebPEncoderBuilder Reset();
+        IWebPEncoderBuilder Reset();
 
         /// <summary>
         /// 建構WebP編碼器
