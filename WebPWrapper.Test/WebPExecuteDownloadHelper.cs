dd05bab0d836ffbc8984593bbf2219f0e717799f
XuPeiYao <st89382000@gmail.com>
2019-05-18 16:30:04 +0800

    加入解碼單元測試以及調整WebP執行檔下載過程

---
+++ WebPWrapper.Test/WebPExecuteDownloadHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace WebPWrapper.Test {
+    public class WebPExecuteDownloadHelper : IDisposable {
+        public WebPExecuteDownloadHelper() {
+            WebPExecuteDownloader.Download();
+        }
+
+        public void Dispose() {
+        }
+
+    }
+
+    [CollectionDefinition("WebP Init")]
+    public class DatabaseCollection : ICollectionFixture<WebPExecuteDownloadHelper> {
+    }
+}
